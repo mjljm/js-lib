688a5bbfb55be378b8da785a713dea301771ec5e
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2023-10-19 17:06:23 +0200

    Initial commit

---
+++ src/regex.ts
@@ -0,0 +1,17 @@
+import * as RA from 'effect/ReadonlyArray';
+
+export const zeroOrMore = (s: string): string => `(?:${s})*`;
+export const oneOrMore = (s: string): string => `(?:${s})+`;
+export const repeatBetween = (s: string, low: string, high = ''): string =>
+	`(?:${s}){${low},${high}}`;
+export const optional = (s: string): string => `(?:${s})?`;
+export const either = (...args: ReadonlyArray<string>): string => `(?:${RA.join(args, '|')})`;
+export const makeLine = (s: string): string => `^${s}$`;
+
+export const star = /\*/.source;
+export const dot = /\./.source;
+export const number = /\d+/.source;
+export const letter = /[A-Za-z]/.source;
+export const slash = /\//.source;
+export const colon = /:/.source;
+export const singleQuote = /'/.source;
---
+++ src/strings.ts
@@ -0,0 +1,39 @@
+/**
+ * Converts camel name to khebab name.
+ *
+ * @param str - The string to convert.
+ *
+ */
+export const kebabize = (str: string) =>
+	str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase());
+
+/**
+ * Converts date in 'YYYY-MM-DD' format to 'YYYYMMDD' format.
+ *
+ * @param s - The string to convert.
+ *
+ */
+export const isoToYyyymmdd = (s: string) => s.slice(0, 4) + s.slice(5, 7) + s.slice(8, 10);
+
+/**
+ * Converts date in 'YYYYMMDD' format 'YYYY-MM-DD' format.
+ *
+ * @param s - The string to convert.
+ *
+ */
+export const yyyymmdToIso = (s: string) =>
+	s.slice(0, 4) + '-' + s.slice(4, 6) + '-' + s.slice(6, 8);
+
+/**
+ * Stringifies parameter u. Returns the error message if stringifies fails
+ *
+ * @param s - The string to convert.
+ *
+ */
+export const stringifyIfPossible = (u: unknown) => {
+	try {
+		return JSON.stringify(u);
+	} catch (e) {
+		return `Could not stringify: ${(e as Error).message}`;
+	}
+};
