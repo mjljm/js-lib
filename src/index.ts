b3981e678d9e7221ed7a13ce4b7f64e946dafc07
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2023-11-17 15:31:31 +0100

    On-going dev

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * as ANSI from '#internal/ansi';
-export * as RegExpUtils from '#internal/reg-exp-utils';
-export * as StringUtils from '#internal/string-utils';
+export * as ANSI from '#mjljm/js-lib/ansi';
+export * as RegExpUtils from '#mjljm/js-lib/reg-exp-utils';
+export * as StringUtils from '#mjljm/js-lib/string-utils';
