d24d16faddbb54cf286bdfc4e153d7b80d541e55
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-03-08 18:47:09 +0100

    On-going dev

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // Move TypedPath to its own library
 export * as TypedPath from '#mjljm/js-lib/TypedPath';
 export * as JsANSI from '#mjljm/js-lib/ansi';
+export * as JsDate from '#mjljm/js-lib/date';
+export * as JsPatches from '#mjljm/js-lib/patches';
 export * as JsRegExp from '#mjljm/js-lib/regexp';
 export * as JsString from '#mjljm/js-lib/string';
 export * as JsTypes from '#mjljm/js-lib/types';
