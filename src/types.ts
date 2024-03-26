33b7668ac8a94666bf43100c31182cf459db858d
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-03-26 16:31:26 +0100

    On-going dev

--- src/types.ts
+++ src/types.ts
@@ -5,10 +5,26 @@
 export type ArrayType<T> = Extract<true extends T & false ? any[] : T extends readonly any[] ? T : unknown[], T>;
 
 /**
- * Type that generates a range of numeric literal types
+ * Utility type that generates a range of numeric literal types
  */
 type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
 	? Acc[number]
 	: Enumerate<N, [...Acc, Acc['length']]>;
 
 export type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
+
+/**
+ * Type utility that extracts all elements of a tuple but the first
+ */
+export type Tail<T extends unknown[]> = T extends [unknown, ...infer R] ? R : never;
+
+/**
+ * Type that expands a type
+ */
+/*export type Develop<P> = P extends TypedPath<infer X, infer Y>
+	? Y extends unknown
+			? X extends unknown
+				? TypedPath<X, Y>
+				: never
+			: never
+		: never*/
